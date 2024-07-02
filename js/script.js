2d519afd3a0434c4a73feb8ef9b42f910e131d7e
Utc1 <flwmsid@gmail.com>
2024-07-02 22:58:51 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function calculate() {
     const count3 = count - count2;
 
     let resultHTML = `<p>개봉 박스 수: ${count3}</p>`;
-    resultHTML += `<p>도달한 박스 번호: ${count-1}</p>`;
+    resultHTML += `<p>도달한 박스 번호: ${count}</p>`;
     resultHTML += `<p>사용한 전화의 개수: ${originalSenka - senka}</p>`;
     resultHTML += `<p>개봉 후 남은 전화의 개수: ${senka}</p>`;
 
